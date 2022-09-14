74f97f3b4405baf8965e33421b85b8115ef458ac
Karina Martinez Guerrero <karinamguerrero@estudiantec.cr>
2022-09-14 17:25:45 -0600

    Calcular determinante

--- tarea1_parte2.jl
+++ tarea1_parte2.jl
@@ -1,5 +1,3 @@
-using LinearAlgebra
-
 function is_pent_diag(M)
     for (i, row) in enumerate(eachrow(M))
         for (j, col) in enumerate(eachindex(row))
@@ -15,26 +13,38 @@ end
 
 function det_penta(M)
     if(is_pent_diag(M))
-        P1 = M[1,1] #P1 = Pi_4
-        P2 = M[2,2]*P1 - M[2,1]*M[1,2] # P2 = Pi_3
-        R2 = M[1,2] #R2 = Ri_3 
-        S2 = M[2,1] #S2 = Si_3
-        P3 = M[3,3]*P2 - M[3,2]*M[2,3]*P1 - M[3,1]*M[1,3]*M[2,2] + M[3,1]*M[2,3]*R2 + M[3,2]*M[1,3]*S2
-        R3 = M[2,3]*P1 - M[1,3]*S2
-        S3 = M[3,2]*P1 - M[3,1]*R2
-        P4 = M[4,4]*P3 - M[4,3]*M[3,4]*P2 - M[4,2]*M[2,4]*(M[3,3]*P1 - M[3,1]*M[1,3]) + M[4,2]*M[3,4]*R3 + M[4,3]*M[2,4]*S3
+        Pi_4 = M[1,1] #P1 = Pi_4
+        Pi_3 = M[2,2]*Pi_4 - M[2,1]*M[1,2] # P2 = Pi_3
+        Ri_3 = M[1,2] #R2 = Ri_3 
+        Si_3 = M[2,1] #S2 = Si_3
+        Pi_2 = M[3,3]*Pi_3 - M[3,2]*M[2,3]*Pi_4 - M[3,1]*M[1,3]*M[2,2] + M[3,1]*M[2,3]*Ri_3 + M[3,2]*M[1,3]*Si_3 #P3 = Pi_2
+        Ri_2 = M[2,3]*Pi_4 - M[1,3]*Si_3 #R3 = Ri_2
+        Si_2 = M[3,2]*Pi_4 - M[3,1]*Ri_3 #S3 = Si_2
+        Pi_1 = M[4,4]*Pi_2 - M[4,3]*M[3,4]*Pi_3 - M[4,2]*M[2,4]*(M[3,3]*Pi_4 - M[3,1]*M[1,3]) + M[4,2]*M[3,4]*Ri_2 + M[4,3]*M[2,4]*Si_2 #P4 = Pi_1
         n = size(M,1)
-        P = Int[]
-        sizehint!(P, n)
+        Pi = 0
         for i = 5:n
-            Ri_1 = M[i-2,i-1]*Pi_3 - M[i-3,i-1]*Si_2,
-            Si_1 = M[i-1,i-2]*Pi_3 - M[i-1,i-3]]*Ri_2,
+            Ri_1 = M[i-2,i-1]*Pi_3 - M[i-3,i-1]*Si_2
+            Si_1 = M[i-1,i-2]*Pi_3 - M[i-1,i-3]*Ri_2
             Pi = M[i,i]*Pi_1 - M[i,i-1]*M[i-1,i]*Pi_2 - 
             M[i,i-2]*M[i-2,i]*(M[i-1,i-1]*Pi_3 - M[i-1,i-3]*M[i-3,i-1]*Pi_4) +
-            M[i,i-2]*M[i-1,i]*Ri_1 + M[i,i-1]*M[i-2,i]*Si_1,
+            M[i,i-2]*M[i-1,i]*Ri_1 + M[i,i-1]*M[i-2,i]*Si_1
+
+            Pi_4 = Pi_3
+            Pi_3 = Pi_2
+            Pi_2 = Pi_1
+            Pi_1 = Pi
+
+            Ri_3 = Ri_2
+            Ri_2 = Ri_1
+            
+            Si_3 = Si_2
+            Si_2 = Si_1
         end
+        return Pi
     else
         print("is not a pentadiagonal")
+        return 0
     end
 end
 
@@ -47,6 +57,4 @@ mat = [
         [0 0 0 3 2 1];
         ]
 
-#print(mat)
-#print(is_pent_diag(mat))
-print(det(mat))
+println(det_penta(mat))
